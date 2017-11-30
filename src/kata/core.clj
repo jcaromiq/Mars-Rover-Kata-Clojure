bf3b274232b201b5ff9405a7e9efcc07b7c5becc
Joaquin Caro <me@joaquin-caro.es>
2017-11-30 16:30:51 +0100

    Refactor: case no commands

--- src/kata/core.clj
+++ src/kata/core.clj
@@ -1,11 +1,10 @@
 (ns kata.core)
 
 (defn move-rover [initial-position commands]
-  (if (clojure.string/blank? commands)
-    initial-position
     (let [x (:x initial-position)
           y (:y initial-position)
           heading (:heading initial-position)]
       (cond
         (and (= heading "N") (= commands "L")) {:x x :y y :heading "W"}
-        (and (= heading "W") (= commands "L")) {:x x :y y :heading "S"}))))
+        (and (= heading "W") (= commands "L")) {:x x :y y :heading "S"}
+        :else initial-position)))
