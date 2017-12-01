53d94a406cae9dacf02410bb251573c7f7a548e7
Joaquin Caro <me@joaquin-caro.es>
2017-12-02 00:17:33 +0100

    Refactor: Add gps to calculate movements

--- src/kata/core.clj
+++ src/kata/core.clj
@@ -1,10 +1,11 @@
 (ns kata.core)
-; Añadir la lista unicamente de puntos cardinales e ir rotandolas
 (def compass {:N {:R "W" :L "E"}
               :E {:R "N" :L "S"}
               :S {:R "E" :L "W"}
               :W {:R "S" :L "N"}})
-
+(def gps {:F
+          {:N {:y 1 :x 0}
+           :E {:y 0 :x 1}}})
 
 (defn- orientate
   [head to]
@@ -13,11 +14,11 @@
     ((keyword to) ((keyword head) compass))))
 
 (defn- move
-  [rover to]
-  (cond
-    (and (="F" to) (= "N" (:heading rover))) (assoc rover :y (+ (:y rover) 1))
-    (and (="F" to) (= "E" (:heading rover))) (assoc rover :x (+ (:x rover) 1))
-    ))
+  [{x :x y :y head :heading :as rover}  to]
+  (let [to-x (:x ((keyword head) ((keyword to) gps)))
+        to-y (:y ((keyword head) ((keyword to) gps)))]
+    (assoc rover :y (+ to-y) :x (+ to-x))))
+
 
 (defn- execute-command
   [{x :x y :y head :heading :as rover}  command]
